ee1d835df87ff669edffc06fef5e07fc7de72adb
Emma Ibarra <eibarram.77@gmail.com>
2024-11-22 11:30:00 -0300

    Actualización datos

--- src/main/java/com/aluracursos/literalura/model/DatosAutor.java
+++ src/main/java/com/aluracursos/literalura/model/DatosAutor.java
@@ -3,9 +3,9 @@ package com.aluracursos.literalura.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosAutor(
         @JsonAlias("name") String nombre,
-        @JsonAlias("birth_year")Integer anoDeNacimiento,
-        @JsonAlias("death_year")Integer anoDeMuerte) {
+        @JsonAlias("birth_year") Integer anoDeNacimiento,
+        @JsonAlias("death_year") Integer anoDeMuerte) {
+
 }
