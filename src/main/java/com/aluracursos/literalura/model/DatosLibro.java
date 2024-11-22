495ee163f0c1a980ffbb32764e70f48ec52f42dc
Emma Ibarra <eibarram.77@gmail.com>
2024-11-22 11:26:35 -0300

    ToString en DatosLibro

--- src/main/java/com/aluracursos/literalura/model/DatosLibro.java
+++ src/main/java/com/aluracursos/literalura/model/DatosLibro.java
@@ -7,8 +7,20 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosLibro(
         @JsonAlias("title") String titulo,
-        @JsonAlias("bookshelves") List<String>genero,
-        @JsonAlias("languages") String idioma,
+        @JsonAlias("authors") List<DatosAutor> autores,
+        @JsonAlias("languages") List<String> idioma,
         @JsonAlias("download_count") Integer numeroDeDescargas) {
+
+    @Override
+    public String toString() {
+        String autor = (autores == null || autores.isEmpty()) ? "Autor desconocido" :
+                autores.get(0).nombre();
+        return  "****Libro****\n" +
+                "Titulo: " + titulo + '\n' +
+                "Autor: " + autor + '\n' +
+                "Idioma: " + idioma.get(0) + '\n' +
+                "Número de descargas: " + numeroDeDescargas + '\n' +
+                "*************" + '\n';
+    }
 
 }
