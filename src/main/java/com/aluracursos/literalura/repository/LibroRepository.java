cd1e48aa6b5a600ab1f27f8f3e3649ed7fee282f
Emma Ibarra <eibarram.77@gmail.com>
2024-11-24 00:50:25 -0300

    Adición de métodos

--- src/main/java/com/aluracursos/literalura/repository/LibroRepository.java
+++ src/main/java/com/aluracursos/literalura/repository/LibroRepository.java
@@ -1,4 +1,17 @@
 package com.aluracursos.literalura.repository;
 
-public interface LibroRepository {
+import com.aluracursos.literalura.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+    boolean existsByTitulo(String titulo);
+
+
+    Optional<Libro> findByTituloContainingIgnoreCase(String titulo);
+
+
 }
