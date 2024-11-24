9a32ebf4e57b0971498cfe0983ea95332e955f64
Emma Ibarra <eibarram.77@gmail.com>
2024-11-24 00:47:13 -0300

    Creación de métodos

---
+++ src/main/java/com/aluracursos/literalura/repository/AutorRepository.java
@@ -0,0 +1,14 @@
+package com.aluracursos.literalura.repository;
+
+import com.aluracursos.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    boolean existsById(Long author_id);
+
+    Optional<Autor> findByNombre(String autorNombre);
+}
