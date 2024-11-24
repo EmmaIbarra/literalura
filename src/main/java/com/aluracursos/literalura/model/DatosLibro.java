e8c76231024b392654e1edb29686c2cc22d97b6f
Emma Ibarra <eibarram.77@gmail.com>
2024-11-24 00:48:26 -0300

    Actualización autor

--- src/main/java/com/aluracursos/literalura/model/DatosLibro.java
+++ src/main/java/com/aluracursos/literalura/model/DatosLibro.java
@@ -11,16 +11,6 @@ public record DatosLibro(
         @JsonAlias("languages") List<String> idioma,
         @JsonAlias("download_count") Integer numeroDeDescargas) {
 
-    @Override
-    public String toString() {
-        String autor = (autores == null || autores.isEmpty()) ? "Autor desconocido" :
-                autores.get(0).nombre();
-        return  "****Libro****\n" +
-                "Titulo: " + titulo + '\n' +
-                "Autor: " + autor + '\n' +
-                "Idioma: " + idioma.get(0) + '\n' +
-                "Número de descargas: " + numeroDeDescargas + '\n' +
-                "*************" + '\n';
-    }
+
 
 }
