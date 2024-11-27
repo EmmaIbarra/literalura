72448e0601b287d7e41c954c9f54f165e47ca8e1
Emma Ibarra <eibarram.77@gmail.com>
2024-11-26 23:59:44 -0300

    eliminación de método

--- src/main/java/com/aluracursos/literalura/repository/LibroRepository.java
+++ src/main/java/com/aluracursos/literalura/repository/LibroRepository.java
@@ -2,16 +2,14 @@ package com.aluracursos.literalura.repository;
 
 import com.aluracursos.literalura.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
-    boolean existsByTitulo(String titulo);
-
-
     Optional<Libro> findByTituloContainingIgnoreCase(String titulo);
-
-
 }
